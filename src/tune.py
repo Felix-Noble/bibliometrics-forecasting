0b9529980391aa97936df5d25e71114521ae7485
Felix Noble <felix.noble@live.co.uk>
2025-08-05 16:43:52 +0100

    optimized via pyarrow row indexing

--- src/tune.py
+++ src/tune.py
@@ -1,13 +1,17 @@
 #src/fit.py
+from os import wait
 from src.utils.load_config import get_model_config, get_log_config, get_data_config, get_train_config
 from src.utils.setup_logging import setup_logger
 from pathlib import Path
 import pandas as pd
 import numpy as np
 # import dask
-import dask.dataframe as dd
-from dask.distributed import Client, LocalCluster, progress
+import pyarrow.compute as pc
+import pyarrow.parquet as pq 
+import pyarrow as pa
+
 import ast
+import time
 import logging
 import duckdb
 import keras
@@ -25,52 +29,89 @@ shuffle_logger.setLevel(logging.ERROR)
 def replace_ref_works(string_val):
     temp = ast.literal_eval(string_val)
     return [int(x.replace("https://openalex.org/W", "")) for x in temp]
-def cols_generator(year, end_year, db_files, cols):
-    df = duckdb.sql(f"""
-    SELECT {", ".join(cols)}
-    FROM read_parquet({db_file})
-    WHERE publication_date_int >= {year} AND publication_date_int < {end_year}
-    """).df()
-    if df.shape[1] < 2:
-        return df.values.reshape(-1,)
-    else:
-        return df.values
-def group_generator(year, end_year, available_ids, db_files, sort_col, n_back, n_features=384):
-    #df = pd.read_parquet(filepath, columns=["id_OpenAlex", "publication_date", "referenced_works_OpenAlex"])
-    df = duckdb.sql(f"""SELECT id_OpenAlex, publication_date, referenced_works_OpenAlex 
+
+def make_id_index_map(db_loc:str, id_col:str = "id_OpenAlex"):
+    database = pq.ParquetDataset(db_loc)
+    id_table = database.read(columns=[id_col])
+    id_index_map = {int(id_val.as_py()): i for i, id_val in enumerate(id_table[id_col])}
+    return id_index_map
+
+def group_generator(db_dir: Path, df, id_index_map:dict, label_map:dict, n_back: int, feature_cols:list): 
+    main_db = pq.read_table(db_dir, memory_map=True)
+    col_lookup_table = main_db.select(feature_cols) 
+    
+    for group_id, group_df in df: 
+        t1 = time.time()
+        ref_works_icol = group_df.columns.get_loc("referenced_works_OpenAlex")
+        indicies = [id_index_map.get(int(id)) for id in group_df.iloc[-n_back:, ref_works_icol]] + [id_index_map.get(group_id)]
+        if not indicies:
+            logger.error(f"No valid indicies found for id : {group_id}")
+        embeddings = pc.take(col_lookup_table, pa.array(indicies, type=pa.int64())).to_pandas()
+        
+        embeddings = embeddings.values.reshape(-1,)
+        if np.isnan(embeddings).any() or np.isinf(embeddings).any():
+            logger.error(f"FATAL: NaN or Inf found in features for group_id {group_id}. Skipping.")
+            print("--------------------------------------------------")
+            print(f"DEBUGGING INFO FOR CORRUPT DATA (group_id: {group_id})")
+            print(f"Type of groupid : {type(group_id)}") 
+            # 1. Show the reference IDs being looked up
+            print("\nReferenced IDs in this group:")
+            print(group_df)
+            
+            # 2. Show the exact row numbers we tried to fetch
+            print("\nInteger row indices passed to pc.take():")
+            print(indicies)
+            
+            # 3. Show the EMBEDDINGS DATAFRAME that contains the NaNs
+            # This is the most important part. It will show you which rows are bad.
+            print("\nEmbeddings DataFrame returned from PyArrow (contains NaNs):")
+            with pd.option_context('display.max_rows', None, 'display.max_columns', None):
+                print(embeddings)
+            print("--------------------------------------------------")
+            time.sleep(10)
+            continue # Skip this sample
+        logger.info(f"Collected, Elapsed = {time.time() - t1}") 
+        yield (np.concatenate([embeddings], dtype=np.float32),label_map.get(group_id, -1))
+
+def create_tf_dataset(batch_size, year, end_year, available_ids, id_index_map, db_files, sort_col, n_back, n_features=384):
+    df = duckdb.sql(f"""SELECT id_OpenAlex, publication_date_int, referenced_works_OpenAlex 
     FROM read_parquet({db_files}) 
     WHERE publication_date_int >= {year} AND publication_date_int < {end_year}""").df()
+
+    df = df.sort_values(sort_col, ascending=True)
+
     df["referenced_works_OpenAlex"] = df["referenced_works_OpenAlex"].apply(replace_ref_works)
     available_ids.columns = ["referenced_works_OpenAlex"] 
     df = df.explode("referenced_works_OpenAlex")
     df = df.merge(available_ids, on="referenced_works_OpenAlex", how="inner")
     df = df.groupby("id_OpenAlex")
     embedding_cols = [f"embedding_{x}" for x in range(n_features)]
-    all_cols_sql = ', '.join([sort_col] + embedding_cols)
-    for group_id, group_df in df: 
-        
-        self_embedding = duckdb.sql(f"SELECT {', '.join(embedding_cols)} FROM read_parquet({db_files}) WHERE id_OpenAlex == {group_id}").df().values.reshape(-1,)
-        print(f"fetching {group_id}") 
-        embeddings = duckdb.sql(f""" 
-        SELECT {all_cols_sql} FROM read_parquet({db_files})
-        WHERE id_OpenAlex IN ({', '.join(group_df['referenced_works_OpenAlex'].astype(str))})""").df()
-        embeddings = embeddings.sort_values(sort_col, ascending=False)[embedding_cols].iloc[-n_back:, :]
-        embeddings = embeddings.values.reshape(-1,)
-
-        yield (np.concatenate([embeddings, self_embedding], dtype=np.float32),)
-
-def create_tf_dataset(batch_size, year, step, available_ids, db_files, sort_col, n_back, n_features=384):
+    y_df = duckdb.sql(f"""
+    SELECT id_OpenAlex, higher_than_median_year 
+    FROM read_parquet({db_files}) 
+    WHERE publication_date_int >= {year} AND publication_date_int < {end_year}
+    """).df()
+    label_map = pd.Series(y_df.higher_than_median_year.values, index=y_df.id_OpenAlex).to_dict()
+    del y_df
+ 
     output_signature = (
         tf.TensorSpec(shape=(None,), dtype=tf.float32),
+        tf.TensorSpec(shape=(), dtype=tf.int32),
     )
     dataset = tf.data.Dataset.from_generator(
-        lambda : group_generator(year, step, available_ids, db_files, sort_col, n_back, n_features),
+    lambda : group_generator(Path(db_files[0]).parent, df, id_index_map, label_map, n_back, embedding_cols),
         output_signature=output_signature,
     )
     dataset = dataset.padded_batch(
         batch_size,
-        padded_shapes=((n_back+1) * n_features,),
-        padding_values=((tf.constant(0, dtype=tf.float32),))
+        padded_shapes=(
+            ((n_back+1) * n_features),       # Shape for 1D feature vector (pad the variable dimension)
+            (),          # Shape for scalar label (it's empty)
+        ),
+        padding_values=(
+             tf.constant(0, dtype=tf.float32),
+            tf.constant(0, dtype=tf.int32), 
+        )
     )
 
     dataset = dataset.prefetch(buffer_size=tf.data.AUTOTUNE)
@@ -79,13 +120,14 @@ def create_tf_dataset(batch_size, year, step, available_ids, db_files, sort_col,
 
 logger = logging.getLogger(Path(__file__).stem)
 
-def process_partitioned_df():
-    pass
-
-
 def main():
-
     setup_logger(logger, get_log_config())
+    gpus = tf.config.list_physical_devices()
+    if gpus:
+        logger.info(f"GPUs found: {len(gpus)}")
+    else:
+        logger.error(f"No GPUs found")
+ 
     data_config = get_data_config()
     model_config = get_model_config()
     train_config = get_train_config()
@@ -93,7 +135,7 @@ def main():
     n_embeddings = data_config["Nembeddings"]
     embedding_cols = [f"embedding_{x}" for x in range(n_embeddings)]
     N_REF = 5
-    n_input = n_embeddings * N_REF 
+    n_input = n_embeddings * (N_REF + 1)
     n_output = 2
     tf.random.set_seed(2025)
     
@@ -108,6 +150,7 @@ def main():
     import os
     database_files = [str(x) for x in Path(database_loc).glob("*.parquet")]
     available_ids = duckdb.sql(f"SELECT id_OpenAlex FROM read_parquet({database_files})").df()
+    id_index_map = make_id_index_map(database_loc, "id_OpenAlex")
 
     first_slice_end_year = start_year + train_config["start_train_size"] + val_size + test_size
 
@@ -123,8 +166,8 @@ def main():
 
         inputs = k_layers.Input(shape=(n_input))
 
-        x = k_layers.Reshape((N_REF, n_embeddings))(inputs)
-        for i in range(hp.Int("conv_layers", 1, 3, default=1)):
+        x = k_layers.Reshape((N_REF+1, n_embeddings))(inputs)
+        for i in range(hp.Int("conv_layers", 1, 2, default=1)):
             x = k_layers.Conv1D (
                 filters = hp.Int("filters_" + str(i), 8, 64, step=8, default=16),
                 kernel_size = hp.Int("kernel_size_" + str(i), 1149, 1915),
@@ -133,7 +176,7 @@ def main():
                 kernel_regularizer=k_reg.l2(l2_regularization)
 
             )(x)
-            x = k_layers.AveragePooling1D()(x)
+            
             x = k_layers.BatchNormalization()(x)
             x = k_layers.ReLU()(x)
         x = k_layers.Flatten()(x)
@@ -159,10 +202,16 @@ def main():
         directory="./cache/",
         project_name=model_config["model_name"],
     )
+    callback = keras.callbacks.EarlyStopping(
+        monitor='val_accuracy',
+        patience=3,
+        mode='max'
+    )
 
     start_year_int = start_year.value
     test_size_int = test_size.value
     val_size_int = val_size.value
+    logger.info(f"{start_year_int} | {test_size_int} | {val_size_int}")
     for current_slice_end_year in range(first_slice_end_year, end_year, CV_delta):
         # for current_slice_end_year in pd.timedelta_range(first_slice_end_year, end_year):
         logger.info(f"Importing slice from temporal range: {previous_slice_end_year} {current_slice_end_year}")
@@ -176,91 +225,19 @@ def main():
         sort_col = "publication_date_int"       
         batch_size = 1000
 
+
         test_start: int = pd.to_datetime(f"{current_slice_end_year}", format="%Y").value - test_size_int
         val_start: int = test_start - val_size_int
-        X_test = create_tf_dataset(batch_size, test_start, test_start + test_size_int, available_ids, database_files, sort_col, N_REF, n_embeddings)
-        X_val = create_tf_dataset(batch_size, val_start, test_start, available_ids, database_files, sort_col, N_REF, n_embeddings)
-        X_train = create_tf_dataset(batch_size, start_year_int, val_start, available_ids, database_files, sort_col, N_REF, n_embeddings)
-        test_train = X_train.as_numpy_iterator() 
-        for x in test_train:
-            print(x)
-        continue 
-        # Safety checks #
-        if len(slice_id_ref) < 1:
-            logger.info("No examples found in range, skipping")
-            continue
 
-        ddf_renamed = ddf.rename(columns={"publication_date":"ref_work_publication_date",
-                                          "referenced_works_count_OpenAlex": "ref_work_referenced_works_count",
-                                          "citation_count_OpenAlex":"ref_work_citation_count",
-                                          })
+        test = create_tf_dataset(batch_size, test_start, test_start + test_size_int, available_ids, id_index_map, database_files, sort_col, N_REF, n_embeddings)
+        val = create_tf_dataset(batch_size, val_start, test_start, available_ids, id_index_map, database_files, sort_col, N_REF, n_embeddings)
+        train = create_tf_dataset(batch_size, start_year_int, val_start, available_ids, id_index_map, database_files, sort_col, N_REF, n_embeddings)
 
-        logger.info("Merging slice ddf with referenced works data")
-        ungrouped_data = dd.merge(
-            ddf_renamed[["ref_work_citation_count", 
-                         "ref_work_referenced_works_count", 
-                         "ref_work_publication_date"] + embedding_cols],
-            slice_id_ref,
-            left_index=True,
-            # left_on="referenced_works_OpenAlex",
-            right_on = "referenced_works_OpenAlex",
-            # right_index=True,
-            how = "inner"
-        )
-        # ungrouped_data = ungrouped_data.persist()
-        ungrouped_data = ungrouped_data.repartition(npartitions=64).persist()
-        progress(ungrouped_data)
-        grouped_embedding_cols = [f"embedding_{x}" for x in range(n_embeddings * N_REFS)]
-        #ungrouped_data = ungrouped_data.set_index("id_OpenAlex").persist()
-        # TODO check for memory availability here, if not start batch mode. TODO write batch train mode
-
-        if All_data is None:
-            logger.debug("No X data found, creating X data from current slice")
-        else:
-            pass
-        if All_data.shape[0] < 1:
-            logger.info("Not enough examples, skipping")
-            continue
-    logger.info(f"Train test split | {start_year} | {val_start} | {test_start} | {current_slice_end_year}")
-
-    # train_ids = (All_data["publication_date"] >= start_year) & (All_data["publication_date"] < val_start)
-    # val_ids = (All_data["publication_date"] >= val_start) & (All_data["publication_date"] < test_start)
-    # test_ids = (All_data["publication_date"] >= test_start)
-
-    #       TODO add caching option for All data to load in pandas df in chunks, then access test train values from these views
-    #       TODO add function to find index of y cols and embedding cols to improve speed here
-    y_col_idx = [All_data.columns.get_loc(x) for x in y_cols]
-    X_col_idx = [All_data.columns.get_loc(x) for x in [y for y in All_data.columns if y.startswith("embedding_")]]
-
-    y_train = All_data.iloc[:val_start_idx, y_col_idx].values.ravel().astype(np.float32)
-    y_val = All_data.iloc[val_start_idx:test_start_idx, y_col_idx].values.ravel().astype(np.float32)
-    y_test = All_data.iloc[test_start_idx:, y_col_idx].values.ravel().astype(np.float32)
-
-    X_train = All_data.iloc[:val_start_idx, X_col_idx].values.astype(np.float32)
-    X_val = All_data.iloc[val_start_idx:test_start_idx, X_col_idx].values.astype(np.float32)
-    X_test = All_data.iloc[test_start_idx:, X_col_idx].values.astype(np.float32)
-
-    logger.info("Data Shapes | total, train, val, test.")
-    logger.info(f"All_data | {All_data.shape} ")
-    logger.info(f"X train | {X_train.shape} | Y train | {y_train.shape}")
-    logger.info(f"X val | {X_val.shape} | Y val| {y_val.shape}")
-    logger.info(f"X test | {X_test.shape} | Y test| {y_test.shape}")
-    logger.debug(All_data.head)
-    logger.debug("Data sucessfully loaded")
-    logger.debug(All_data)
-    previous_slice_end_year = current_slice_end_year
-
-
-    #mlflow_logger = keras_tuner.integration.mlflow.MLflowLogger(tuner)
-
-    tuner.search(X_train, y_train, epochs=15, validation_data=(X_val, y_val))
-    models = tuner.get_best_models(num_models=2)
-    best_model = models[0]
-    best_model.summary()
-
-    BA = None
-    Prec = None
-    roc_auc=None
+        logger.info("Starting search...")
+        tuner.search(train, epochs=5, validation_data=val, callbacks=[callback])
+        models = tuner.get_best_models(num_models=2)
+        best_model = models[0]
+        best_model.summary()
 
 # best_hps = tuner.get_best_hyperparameters(num_trials=1)[0]
 
